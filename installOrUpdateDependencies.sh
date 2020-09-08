cacc862e833f8937e0b3035b74ff612c5cbf68bf
Florian Heinze <florian.heinze@sandstorm.de>
2020-09-08 12:57:03 +0200

    TASK: back to using the fork afain

--- installOrUpdateDependencies.sh
+++ installOrUpdateDependencies.sh
@@ -26,15 +26,14 @@ brew services stop yubikey-agent &> /dev/null && echo "Service was stopped" || e
 # we make sure to uninstall the old fork here or an older version
 brew uninstall yubikey-agent &> /dev/null && echo "Agent was uninstalled" || echo "Nothing to uninstall"
 
-green_echo "STEP 4: installing yubikey-agent sandstorm fork"
-brew tap filippo.io/yubikey-agent https://filippo.io/yubikey-agent &> /dev/null && echo "filippo.io/yubikey-agent was taped" || echo "Nothing to tap"
-brew install yubikey-agent &> /dev/null && echo "Agent was reinstalled" || echo "Reinstall failed"
-echo ""
-brew services start yubikey-agent
-
-
-# In case we need the fork again ;)
+# Currently still using the fork until the formula is updated
 # As most people are using `sandstorm/tap` for the sku-tools. We changed the name
 # of the yubikey-agent fork to yubikey-agent-sandstorm as all formulas will be present
 # and brew would otherwise fail installing the original yubikey-agent.
-# brew install sandstorm/tap/yubikey-agent-sandstorm
+brew install sandstorm/tap/yubikey-agent-sandstorm
+
+green_echo "STEP 4: installing yubikey-agent sandstorm fork"
+# brew tap filippo.io/yubikey-agent https://filippo.io/yubikey-agent &> /dev/null && echo "filippo.io/yubikey-agent was taped" || echo "Nothing to tap"
+# brew install yubikey-agent &> /dev/null && echo "Agent was reinstalled" || echo "Reinstall failed"
+echo ""
+brew services start yubikey-agent-sandstorm
