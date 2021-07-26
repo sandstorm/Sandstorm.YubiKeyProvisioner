384d57e59d2c38af40a55532c395093759d8942e
Tobias Gruber <tobias.gruber@sandstorm.de>
2021-07-26 17:35:53 +0200

    FIX: CN error during certificate creation and remove deprecation warnings

--- resetAndProvisionYubiKey.sh
+++ resetAndProvisionYubiKey.sh
@@ -1,38 +1,28 @@
 #!/usr/bin/env bash
 set -e
-
 default_management_key="010203040506070801020304050607080102030405060708"
 default_pin="123456"
 default_puk="12345678"
-
 # we generate a random PIN and PUK here to enforce safety
 # and have the script run through correctly. The user will be prompted
 # to safe PIN and PUK to the vault
-
 currentUser=`whoami`
-
 # We need to know where brew was installed to use the correct location for
 # the yubikey socket. This can be different depending on processor generation and OS version
 brewPrefix=$(brew --prefix)
-
 green_echo() {
   printf "\033[0;32m%s\033[0m\n" "${1}"
 }
-
 yellow_echo() {
   printf "\033[1;33m%s\033[0m\n" "${1}"
 }
-
 red_echo() {
   printf "\033[0;31m%s\033[0m\n" "${1}"
 }
-
 grey_echo() {
   printf "\033[0;37m%s\033[0m\n" "${1}"
 }
-
 # RESETTTING
-
 green_echo "STEP 1 - Resetting YubiKey"
 echo
 echo "-> Reset Fido"
@@ -43,7 +33,7 @@ ykman oath reset --force
 echo
 echo "-> Reset Piv"
 ykman piv reset --force
-ykman piv set-pin-retries  3 3 \
+ykman piv access set-retries  3 3 \
   --pin "123456" \
   --management-key $default_management_key \
   --force
@@ -51,15 +41,12 @@ echo
 echo
 green_echo "STEP 2 - Setting YubiKey Mode"
 echo "enables all interfaces except OTP"
-ykman mode FIDO+CCID
+ykman config mode FIDO+CCID
 echo
 echo
-
 # MANAGEMENT KEY
-
 green_echo "STEP 3 - Setting A Management Key"
 yellow_echo "Please provide the company-wide management key. Ask your Admin"
-
 while true; do
     read -r -s -p "management key: " management_key
     echo
@@ -69,15 +56,13 @@ while true; do
     echo "Please try again"
 done
 echo
-ykman piv change-management-key \
+ykman piv access change-management-key \
   --touch --new-management-key "$management_key" \
   --management-key "$default_management_key" \
   || { red_echo "Make sure that you do NOT run this script for an already provisioned YubiKey!!!"; exit; }
 echo
 echo
-
 # PUK AND PIN
-
 green_echo "STEP 4 - Generating & Setting A PUK And PIN"
 puk_pattern='^[a-zA-Z0-9]{8}$'
 pin_pattern='^[a-zA-Z0-9]{6}$'
@@ -126,16 +111,13 @@ yellow_echo "  * Also store the Serial Number of your YubiKey. This is as list o
 echo "    * $(ykman list)"
 echo
 read -r -p "Press enter to continue. PUK and PIN will be provisioned."
-
-ykman piv change-puk --puk $default_puk --new-puk "$puk" \
+ykman piv access change-puk --puk $default_puk --new-puk "$puk" \
   || { red_echo "Make sure that you do NOT run this script for an already provisioned YubiKey!!!"; exit; }
-ykman piv change-pin --pin $default_pin --new-pin "$pin" \
+ykman piv access change-pin --pin $default_pin --new-pin "$pin" \
   || { red_echo "Make sure that you do NOT run this script for an already provisioned YubiKey!!!"; exit; }
 echo
 echo
-
 # SHH KEYS
-
 green_echo "STEP 5 - Generating SSH Keys"
 echo "Please enter your personal information."
 echo "It will be used as the Subject common name (CN) for the certificate like so 'CN=SSH for {first_name} {last_name}'"
@@ -145,26 +127,22 @@ read -r -p "Last Name: " last_name
 echo
 rm -rf generated &> /dev/null
 mkdir -p generated &> /dev/null
-
 pushd ./generated &> /dev/null
 echo "-> generate private key on yubikey to be used for SSH"
 echo "   this might take a while, keep tapping the yubikey"
-ykman piv generate-key --management-key "$management_key" --touch-policy CACHED --pin-policy ONCE 9a public.pem
+ykman piv keys generate --management-key "$management_key" --touch-policy CACHED --pin-policy ONCE 9a public.pem
 echo "-> generate self-signed certificate for that key"
 echo "   this might take a while, keep tapping the yubikey"
-ykman piv generate-certificate --management-key "$management_key" --pin "$pin" -d 3650 -s "/CN=SSH for $first_name $last_name/" 9a public.pem
+ykman piv certificates generate --management-key "$management_key" --pin "$pin" -d 3650 -s "CN=SSH for $first_name $last_name" 9a public.pem
 rm public.pem
 echo "-> extract public SSH key from YubiKey"
 pkcs15-tool --read-ssh-key 01 | sed -e "s/PIV AUTH pubkey/$currentUser@YubiKey/g" > "$currentUser".yubikey.pub
 echo
 echo
-
 # DISPLAYING HELP FOR MANUAL STEPS
-
 green_echo "STEP 6 - Manual Steps to finalize"
 echo
 yellow_echo "  * Register your YubiKey at auth.sandstorm.de"
-yellow_echo "  * For Firefox enable U2F support"
 yellow_echo "  * Copy the generated public key to your ssh directory: cp ./generated/$currentUser.yubikey.pub ~/.ssh/"
 yellow_echo "  * Add the following lines to your ~/.ssh/config to make sure the YubiKey SSH Agent is used for all SSH connections."
 echo
@@ -194,4 +172,3 @@ echo
 yellow_echo "  * For more information check out https://github.com/FiloSottile/yubikey-agent"
 echo
 green_echo "DONE"
-
