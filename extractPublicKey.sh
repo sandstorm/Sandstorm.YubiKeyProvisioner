ca5a4bc70e880e85f27c2165872631555408188f
Martin Hauke <martin.hauke@sandstorm.de>
2021-11-27 14:53:25 +0100

    Add documentation and override safeguard

--- extractPublicKey.sh
+++ extractPublicKey.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 USERNAME=$(whoami)
+FILENAME="$USERNAME".yubikey.pub
 
 # stop yubikey agent - necessary to access the yubikey with pkcs
 brew services stop sandstorm-yubikey-agent
 
 # extract public key | replace string at the end with identifier > save it in ~/.ssh/
-pkcs15-tool --read-ssh-key 01 | sed -e "s/PIV AUTH pubkey/$USERNAME@YubiKey/g" > ~/.ssh/"$USERNAME".yubikey.pub
+pkcs15-tool --read-ssh-key 01 | sed -e "s/PIV AUTH pubkey/$USERNAME@YubiKey/g" > "$FILENAME"
+mv -i "$FILENAME" ~/.ssh/"$FILENAME"
 
 # start yubikey agent again
 brew services start sandstorm-yubikey-agent
