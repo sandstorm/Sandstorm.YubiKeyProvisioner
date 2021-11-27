b3752eb439400da36dad31a6061101b2475b4c03
Martin Hauke <martin.hauke@sandstorm.de>
2021-11-27 15:07:40 +0100

    Add cleanup in case moving fails

--- extractPublicKey.sh
+++ extractPublicKey.sh
@@ -5,9 +5,15 @@ FILENAME="$USERNAME".yubikey.pub
 # stop yubikey agent - necessary to access the yubikey with pkcs
 brew services stop sandstorm-yubikey-agent
 
-# extract public key | replace string at the end with identifier > save it in ~/.ssh/
+# extract public key
 pkcs15-tool --read-ssh-key 01 | sed -e "s/PIV AUTH pubkey/$USERNAME@YubiKey/g" > "$FILENAME"
+# use mv -i to avoid accidental overrides
 mv -i "$FILENAME" ~/.ssh/"$FILENAME"
+
+# clean up, in case the mv command fails
+if test -f "$FILENAME"; then
+  rm "$FILENAME"
+fi
 
 # start yubikey agent again
 brew services start sandstorm-yubikey-agent
